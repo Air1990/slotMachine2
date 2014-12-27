8612a55c6fe7bd06224797d89a6594192f558174
明阳 <lmysoar@hotmail.com>
2014-12-27 16:03:18 +0800

    first commit

--- public/javascripts/action.js
+++ public/javascripts/action.js
@@ -1,55 +1,89 @@
-var createReel = function() {
+var Reel = function(t) {
     var radius = 300;
-    
-    var obj = new THREE.Object3D();
-    
+
+    this.obj = new THREE.Object3D();
+
+    this.target = t || 0;   // the target number to stop at
+
+    this.Omiga = 0.20;      // max angle speed
+    this.omiga = 0;         // current angle speed
+    this.beta  = 0.0005;    // angle speed's speed when speed up
+    this.beta2 = 0.0005;    // angle speed's speed when speed down
+
+    var action = 0;        // 0 -- running; 1 -- stop
+
     for (var i = 0; i < 10; ++i) {
         var div = document.createElement('div');
         div.className = 'digit';
-        
+
         var span = document.createElement('span');
         span.textContent = i.toString();
         div.appendChild(span);
-        
+
         var digit = new THREE.CSS3DObject(div);
         var r = Math.PI * i / 5;
         digit.position.x = 0;
         digit.position.y = radius * Math.sin(r);
         digit.position.z = radius * Math.cos(r);
         digit.rotation.x = -r;
-        
-        obj.add(digit);
+
+        this.obj.add(digit);
     }
-    
-    obj.renew = function() {
+
+    this.update = function() {
         var alpha = 0;
-        for (var i = 0, r = obj.rotation.x; i < 10; ++i, r -= Math.PI/5) {
+        this.obj.rotation.x += this.omiga;
+        var r = this.obj.rotation.x;
+        for (var i = 0; i < 10; ++i, r -= Math.PI/5) {
             alpha = 0.4 + 0.5 * Math.cos(r);
-            obj.children[i].element.style.opacity = alpha.toString();
-            obj.children[i].element.style.filter = 'alpha(opacity='+(alpha*100).toString()+')';
-            //r -= Math.PI/5;
+            this.obj.children[i].element.style.opacity = alpha.toString();
+            this.obj.children[i].element.style.filter = 'alpha(opacity='+(alpha*100).toString()+')';
+        }
+        if (action == 0) {
+            if (this.omiga < this.Omiga) {
+                this.omiga += this.beta;
+            }
+        } else if (action == 1) {
+            if (this.omiga > 0) {
+                this.omiga -= this.beta2;
+            }
         }
     };
-    
-    return obj;
+
+    this.run = function () {
+        action = 0;
+    };
+
+    this.stop = function() {
+        var phi = this.target * Math.PI / 5;
+        this.beta2 = (this.omiga * this.omiga) /
+        (40 * Math.PI + 2 * (phi - this.obj.rotation.x) % (2 * Math.PI));
+        console.log(this.beta2);
+        action = 1;
+    };
+
+    this.isRunning = function() {
+        return action == 0;
+    };
 };
 
-window.onload = function() {
+var start = function() {
     var scene = new THREE.Scene();
-    
+
     var reels = [];
     for (var i = 0; i < 8; ++i) {
-        var reel = createReel();
-        reel.position.x = 140 * i - 490;
-        reel.rotation.x = 0;
-        scene.add(reel);
+        var reel = new Reel(i);
+        reel.obj.position.x = 140 * i - 490;
+        reel.obj.rotation.x = 0;
+
+        scene.add(reel.obj);
         reels.push(reel);
     }
-    
+
     var renderer = new THREE.CSS3DRenderer({
         antialias: true
     });
-    
+
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
@@ -59,32 +93,47 @@ window.onload = function() {
         window.innerWidth / window.innerHeight,
         0.1, 1000
     );
-//    var camera = new THREE.OrthographicCamera(
-//        75,
-//        window.innerWidth / window.innerHeight,
-//        0.1, 1000
-//    );
     camera.position.z = 1200;
 
+    //var v = [.010, .015, .020, .025, .030, .035, .040, .045];
+    var v = [.015, .015, .015, .015, .015, .015, .015, .015];
     var render = function() {
         requestAnimationFrame(render);
-        for(var i in reels) {
-            reels[i].rotation.x += 0.010;//+0.003*i;
-            reels[i].renew();
+
+        for (var i = 0; i < 8; ++i) {
+            reels[i].update();
         }
+
+        //document.getElementById('log').innerHTML = Math.round(reels[7].rotation.x).toString();
+
         renderer.render(scene, camera);
     };
 
     render();
-    
-    // testing
-//    var bg = document.getElementById('bg');
-//    var ctx = bg.getContext('2d');
-//    
-//    ctx.fillStyle = 'rgba(200,200,200,1)';
-//    ctx.fillRect(0, 0, 200, 40);
-    
+    window.onkeydown = function (event) {
+        if(event.keyCode = 13) {
+            reels.forEach(function(ele, index) {
+                console.log(index);
+                setTimeout(function () {
+                    ele.stop();
+                },1000*index);
+            });
+        }
+    };
+    //window.addEventListener('keydown', function() {
+    //    for (var i = 0; i < 8; ++i) {
+    //        //reels[i].action = 1^reels[i].action;
+    //        //if (reels[i].isRunning()) {
+    //        reels[i].stop();
+    //        //} else {
+    //        //    reels[i].run();
+    //        //}
+    //    }
+    //});
+
     window.addEventListener('resize', function() {
         renderer.setSize(window.innerWidth, window.innerHeight);
     }, false);
 };
+
+window.onload = start;
